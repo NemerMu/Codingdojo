966c683ed9e3c9e38ea7d4b1bac37dc7a18eee6a
NemerMu <78445684+NemerMu@users.noreply.github.com>
2021-04-21 01:59:21 -0400

    Donation Tracker

---
+++ Final Project/Donation Tracker/test/test.js
@@ -0,0 +1,21 @@
+const donationT = artifacts.require("donationT");
+
+contract("donationT", async function(accounts){
+
+    it (
+    "should add Organization", async function(){
+    let instance = await donationT.deployed()
+    await instance.registerOrganization( "ong", "u");//.send({from: this.account });
+  // orgRegister[regCounter]  
+    });
+
+     it('should execute only by the owner', async()=>{
+        let instance = await donationT.deployed()
+        await instance.verifyOrganization(1);// {'from': accounts[0]});
+        await instance.getPastEvents("allEvents", function(error, events){ console.log(events[0].returnValues.orgName, events[0].returnValues.orgCategory, events[0].returnValues.verificationTime);})
+        //const value = await instance.verifyOrg(); //or getorg
+     //_verifyOrganization(string  orgName, string  orgCategory, uint verificationTime);
+      //  assert.equal(value, 'modifier');
+      });
+
+    });;
